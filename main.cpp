4a227a8498bcef9c24be27d238bcfb50821ec4c5
Jb-rown <brownjohn9870@gmail.com>
2025-04-21 11:58:12 +0300

    Introduce syntax error to test failing build

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ void showMenu() {
 
 
 int main() {
-    TaskManager manager;
-    string filename = "tasks.txt";
+    TaskManager manager
+    string filename = "tasks.txt"; // missing semicolon above
 
     manager.loadFromFile(filename);
 
